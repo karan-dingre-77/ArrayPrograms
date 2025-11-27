78f8992305e6f2355711f4371d2111179e47d686
karan-dingre-77 <karan.dingre77@gmail.com>
2025-11-27 23:05:04 +0530

    add new program

--- ArrayDay6/PrefixSum.java
+++ ArrayDay6/PrefixSum.java
@@ -11,9 +11,9 @@ public class PrefixSum {
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        prefixSum(arr);
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+       int[] ans = prefixSum(arr);
+        for(int i=0; i<ans.length; i++){
+            System.out.print(ans[i] + " ");
         }
     }
 }
